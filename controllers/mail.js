183854b31f4bececbc86186e5efede92476cedf1
ErKiran <kiruu1238@gmail.com>
2020-07-31 11:27:27 +0545

    Dynamic Templates

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,35 +1,55 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const { promisify } = require("util");
 
-async function initNodemailer(){
-    const {EMAIL,PASSWORD} = process.env
-    let transporter = nodemailer.createTransport({
-        host:'smtp.mailtrap.io',
-        port:25,
-        auth:{
-            user: EMAIL,
-            pass: PASSWORD
-        }
-    })
+async function initNodemailer() {
+  const { EMAIL, PASSWORD } = process.env;
+  let transporter = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 25,
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD,
+    },
+  });
 
-    return transporter
+  return transporter;
 }
 
-async function verifyAccount(email,code){
-    try{
-        const transporter = await initNodemailer()
-       await transporter.sendMail({
-            from:`Krishi Kranti <krishi@gmail.com>`,
-            to: email,
-            subject:"Verify Account",
-            text:"Verify Email",
-            html: {path:'./templates/mail.html'},
-        })
-    }
-    catch(err){
-        throw new Error(`Can't Send Mail to verify Account ${err}`)
-    }
+async function compileTemplate(email,code) {
+  try {
+    const readFileAsync = promisify(fs.readFile);
+    const html = await readFileAsync("./templates/mail.html", {
+      encoding: "utf-8",
+    });
+    const template = handlebars.compile(html);
+    const replacements = {
+      username: email.split('@')[0],
+      code
+    };
+    return template(replacements);
+  } catch (err) {
+    throw new Error(`Can't compile Template ${err}`);
+  }
+}
+
+async function verifyAccount(email, code) {
+  try {
+    const transporter = await initNodemailer();
+    const html = await compileTemplate(email,code);
+    await transporter.sendMail({
+      from: `Krishi Kranti <krishi@gmail.com>`,
+      to: email,
+      subject: "Verify Account",
+      text: "Verify Email",
+      html,
+    });
+  } catch (err) {
+    throw new Error(`Can't Send Mail to verify Account ${err}`);
+  }
 }
 
 module.exports = {
-    verifyAccount
-}
+  verifyAccount,
+};
